05e5ddf7ce10eaa0089b9035d037dd861a7183a1
siemens-dbenoit <david.benoit@siemens.com>
2022-03-10 11:15:58 +0100

    add solution for "TIA Saver"

---
+++ Solutions/TIASaver/TIASaver/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace TIASaver
+{
+    partial class FMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnectToTIA = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnArchive = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnectToTIA
+            // 
+            this.btnConnectToTIA.Location = new System.Drawing.Point(11, 9);
+            this.btnConnectToTIA.Name = "btnConnectToTIA";
+            this.btnConnectToTIA.Size = new System.Drawing.Size(190, 47);
+            this.btnConnectToTIA.TabIndex = 0;
+            this.btnConnectToTIA.Text = "Connect to TIA Portal";
+            this.btnConnectToTIA.UseVisualStyleBackColor = true;
+            this.btnConnectToTIA.Click += new System.EventHandler(this.btnConnectToTIA_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(12, 77);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(189, 47);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Enabled = false;
+            this.btnSaveAs.Location = new System.Drawing.Point(12, 130);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(189, 47);
+            this.btnSaveAs.TabIndex = 2;
+            this.btnSaveAs.Text = "Save As...";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnArchive
+            // 
+            this.btnArchive.Enabled = false;
+            this.btnArchive.Location = new System.Drawing.Point(13, 183);
+            this.btnArchive.Name = "btnArchive";
+            this.btnArchive.Size = new System.Drawing.Size(188, 47);
+            this.btnArchive.TabIndex = 3;
+            this.btnArchive.Text = "Create Archive";
+            this.btnArchive.UseVisualStyleBackColor = true;
+            this.btnArchive.Click += new System.EventHandler(this.btnArchive_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(187, 10);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 233);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnArchive);
+            this.Controls.Add(this.btnSaveAs);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnConnectToTIA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FMain";
+            this.Text = "TIA Saver";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnectToTIA;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnArchive;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Solutions/TIASaver/TIASaver/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// ++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++
+// TODO:
+// en:
+// include using-directives here!
+// de:
+// using-Anweisung hier einfügen!
+using Siemens.Engineering;
+using Siemens.Engineering.Hmi;
+// ++++++++++++++++++++++++++++++++++
+// ++++++++++++++++++++++++++++++++++
+
+namespace TIASaver
+{
+    public partial class FMain : Form
+    {
+
+        // ++++++++++++++++++++++++++++++++++
+        // ++++++++++++++++++++++++++++++++++
+        // TODO:
+        // en:
+        // declare the globale variables here
+        // de:
+        // hier die globalen Variablen deklarieren
+        public TiaPortalProcess MyTiaPortalInstance;
+        public TiaPortal MyPortal;
+        public Project MyOpenProject;
+        public string ArchivePath = null;
+        // ++++++++++++++++++++++++++++++++++
+        // ++++++++++++++++++++++++++++++++++
+
+
+        public FMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnectToTIA_Click(object sender, EventArgs e)
+        {
+            if (btnConnectToTIA.Text == "Connect to TIA Portal")
+            {
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                // TODO:
+                // en:
+                // Get all running TIA instances
+                // connect to the process
+                // check if a Project is opened
+                // if yes, assign it to the variable MyOpenProject
+                // if not, assign null to the variable MyOpenProject
+                // de:
+                // Laufende Portale ermitteln
+                // mit Prozess verbinden
+                // Prüfung, ob ein Projekt geöffnet ist
+                // Wenn ja, an die Variable MyOpenProject zuweisen
+                // Wenn nein, der Variablen MyOpenProject ein null zuweisen
+                IList<TiaPortalProcess> MyRunningTias = TiaPortal.GetProcesses();
+                // Given the fact that only one TIA is running, otherwise handle as in slide.
+                if (MyRunningTias.Count == 0)
+                {
+                    return;
+                }
+                MyTiaPortalInstance = MyRunningTias[0];
+                MyPortal = MyTiaPortalInstance.Attach();
+
+                // Given the fact that only one Project is opened
+                if (!(MyPortal.Projects.Count == 0))
+                {
+                    MyOpenProject = MyPortal.Projects[0];
+                }
+                else
+                {
+                    MyOpenProject = null;
+                }
+
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                btnSave.Enabled = true;
+                btnSaveAs.Enabled = true;
+                btnArchive.Enabled = true;
+                btnConnectToTIA.Text = "Disconnect from TIA Portal";
+            }
+            else
+            {
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                // TODO:
+                // en:
+                // Disconnect from TIA Portal
+                // de:
+                // Verbindung zum TIA Portal trennen
+                MyPortal.Dispose();
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                btnSave.Enabled = false;
+                btnSaveAs.Enabled = false;
+                btnArchive.Enabled = false;
+                btnConnectToTIA.Text = "Connect to TIA Portal";
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // ++++++++++++++++++++++++++++++++++
+            // ++++++++++++++++++++++++++++++++++
+            // TODO:
+            // en:
+            // Check if a Project is opened
+            // if yes, save the Project
+            // de:
+            // Abfrage, ob ein Projekt geöffnet ist
+            // Wenn ja, dann Projekt speichern
+            if (!(MyOpenProject == null))
+            {
+                MyOpenProject.Save();
+            }
+            // ++++++++++++++++++++++++++++++++++
+            // ++++++++++++++++++++++++++++++++++
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            if (MyOpenProject == null)
+            {
+                return;
+            }
+            FolderBrowserDialog MyBrowserDialog = new FolderBrowserDialog();
+            MyBrowserDialog.SelectedPath = MyOpenProject.Path.ToString();
+            MyBrowserDialog.Description = "Please select an empty folder to save";
+            if (MyBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                // TODO:
+                // en:
+                // Perform a "Save as" to the location that was selected via the Dialog
+                // Access to the selected folder: MyBrowserDialog.SelectedPath
+                // de:
+                // "Speichern unter" durchführen auf den leeren Ordner, der vom Dialog ausgewählt wurde
+                // Zugriff auf ausgewählten Ordner: MyBrowserDialog.SelectedPath
+                MyOpenProject.SaveAs(new System.IO.DirectoryInfo(MyBrowserDialog.SelectedPath));
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+            }
+
+        }
+
+        private void btnArchive_Click(object sender, EventArgs e)
+        {
+            if (MyOpenProject == null)
+            {
+                return;
+            }
+            if (ArchivePath == null)
+            {
+                FolderBrowserDialog MyBrowserDialog = new FolderBrowserDialog();
+                MyBrowserDialog.SelectedPath = MyOpenProject.Path.ToString();
+                MyBrowserDialog.Description = "Please select an empty folder to save";
+                if (MyBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    ArchivePath = MyBrowserDialog.SelectedPath;
+                }
+            }
+            string ProjectName = MyOpenProject.Name +"_" +System.DateTime.Now.ToShortDateString() + "__"
+                +System.DateTime.Now.ToShortTimeString().Replace(":","_")+".zap16";
+            if (ArchivePath!=null & !System.IO.File.Exists(ArchivePath + "\\"+ MyOpenProject.Name + "_" 
+                + System.DateTime.Now.ToShortDateString() + "__" + System.DateTime.Now.ToShortTimeString().Replace(":", "_") + ".zap16"))
+            {
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+                // TODO:
+                // en:
+                // Archive the opened Project.
+                // The project should be archived in folder ArchivePath, 
+                // the name should be ProjectName (not as a solid string, but the variable!) 
+                // and it should be compressed with discard of restorable data
+                // de:
+                // Das geöffnete Projekt soll archiviert werden
+                // Dies soll in den Ordner ArchivePath geschehen,
+                // der Name soll durch die Variable ProjectName angegeben werden.
+                // Das Projekt soll komprimiert werden und die wiederherstellbaren Daten gelöscht werden.
+                MyOpenProject.Archive(new System.IO.DirectoryInfo(ArchivePath), ProjectName , ProjectArchivationMode.DiscardRestorableDataAndCompressed);
+                // ++++++++++++++++++++++++++++++++++
+                // ++++++++++++++++++++++++++++++++++
+            }
+        }
+    }
+}
